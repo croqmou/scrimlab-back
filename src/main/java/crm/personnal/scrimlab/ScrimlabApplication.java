a3c947c5efef982a586ba64156e713aca931d9f4
croqmou <croqmou7@gmail.com>
2025-05-29 15:45:12 +0200

    :tada: project initialisation

---
+++ src/main/java/crm/personnal/scrimlab/ScrimlabApplication.java
@@ -0,0 +1,13 @@
+package crm.personnal.scrimlab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScrimlabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScrimlabApplication.class, args);
+	}
+
+}
