43b3af044d6fd4ff0eb4501efe60deaeb2d35671
Leo Fantuz Chrobot <leof150105@gmail.com>
2025-11-20 17:49:10 +0100

    ajout de la page team profile

---
+++ src/main/resources/db/changelog/local/add-players.sql
@@ -0,0 +1,9 @@
+--changeset Scrimlab:BTS-28-local
+
+INSERT INTO Player VALUES('croqmou7@gmail.com','CRM','$2a$10$IBMlYkZG6yOuXzFpYlkdh.yHhERAK/ogw2MVhgrWVCZbkLPSomyC6','unknown.png',false,0,0,0,0,0,0);
+INSERT INTO Player VALUES('lucasf150105@gmail.com','ShinyWolf','$2a$10$IBMlYkZG6yOuXzFpYlkdh.yHhERAK/ogw2MVhgrWVCZbkLPSomyC6','unknown.png',false,0,0,0,0,0,0);
+INSERT INTO Player VALUES('wars@gmail.com','Wars','$2a$10$IBMlYkZG6yOuXzFpYlkdh.yHhERAK/ogw2MVhgrWVCZbkLPSomyC6','unknown.png',false,0,0,0,0,0,0);
+INSERT INTO Player VALUES('mayto@gmail.com','Mayto','$2a$10$IBMlYkZG6yOuXzFpYlkdh.yHhERAK/ogw2MVhgrWVCZbkLPSomyC6','unknown.png',false,0,0,0,0,0,0);
+INSERT INTO Player VALUES('tens@gmail.com','Tens','$2a$10$IBMlYkZG6yOuXzFpYlkdh.yHhERAK/ogw2MVhgrWVCZbkLPSomyC6','unknown.png',false,0,0,0,0,0,0);
+INSERT INTO Player VALUES('grimflam@gmail.com','Grimflam','$2a$10$IBMlYkZG6yOuXzFpYlkdh.yHhERAK/ogw2MVhgrWVCZbkLPSomyC6','unknown.png',false,0,0,0,0,0,0);
+INSERT INTO Player VALUES('wertag@gmail.com','Wertag','$2a$10$IBMlYkZG6yOuXzFpYlkdh.yHhERAK/ogw2MVhgrWVCZbkLPSomyC6','unknown.png',false,0,0,0,0,0,0);
---
+++ src/main/resources/db/changelog/local/add-team.sql
@@ -0,0 +1,3 @@
+--changeset Scrimlab:BTS-28-2-local
+
+INSERT INTO Team VALUES('SNIR ESPORT','6a55161a-941f-47cc-b20b-7735e33867b3_snir.jpeg','54cffa65-929c-4550-a6c7-487f4971cec2_EN.png','Snir goatesque','croqmou7@gmail.com','lucasf150105@gmail.com','wars@gmail.com','mayto@gmail.com','tens@gmail.com','grimflam@gmail.com','wertag@gmail.com');
